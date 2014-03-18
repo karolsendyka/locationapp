325ff1fac6fc084cfdd1dd70bea37300223da270
Karol Sendyka <karol.sendyka@sabre.com>
2014-03-18 20:16:40 +0100

    created function to add map markers on click event

--- src/main/webapp/map/map.js
+++ src/main/webapp/map/map.js
@@ -1,9 +1,21 @@
+var map;
+
+function placeMarker(location) {
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
+
 function initialize() {
     var mapOptions = {
         center: new google.maps.LatLng(50, 15),
         zoom: 5
     };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-        mapOptions);
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+    google.maps.event.addListener(map, 'click', function (event) {
+        placeMarker(event.latLng);
+    });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
